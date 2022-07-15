1474ccec7a6dfd2ffdcf032ae6874c5e876d5127
Omid Reza <omid77.orh@gmail.com>
2022-07-15 16:08:33 +0430

    Update Alpha.m
    Fix style of the plot

--- display/connectivity/active_ec/Alpha.m
+++ display/connectivity/active_ec/Alpha.m
@@ -1,6 +1,6 @@
 clear;
 % Config plot
-plot.dot_size=60;
+plot.dot_size=100;
 plot.y.pre.data=load("result\connectivity\active_ec\pre\Alpha.txt");
 plot.y.post.data=load("result\connectivity\active_ec\post\Alpha.txt");
 plot.y.pre.color=[57 62 70].*4/1000;
@@ -15,5 +15,7 @@ swarmchart(plot.x.post.data, plot.y.post.data, plot.dot_size, plot.y.post.color,
 legend("Pre", "Post");
 % Config plot
 set(gca,'XTick',[]);
-ylabel("Alpha Connectivity", "Color", [21 19 60].*4/1000,"FontWeight", "bold");
+ylabel("Alpha Connectivity", "Color", [21 19 60].*4/1000, "FontWeight", "bold");
 set(gcf, "NumberTitle", false, "Name", "Active - EC");
+set(gca, 'GridColor', [26 77 46].*4/1000);
+set(gca, 'YColor', [26 77 46].*4/1000);
