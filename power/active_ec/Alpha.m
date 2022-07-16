842f7ca579b2773d8cd191ca3c224ed68d64b325
Omid Reza <omid77.orh@gmail.com>
2022-07-16 22:26:41 +0430

    Extend analyze
    Extend analyze spots & extend analyze for post measures

--- power/active_ec/Alpha.m
+++ power/active_ec/Alpha.m
@@ -1,28 +1,43 @@
 clear;
 load data\data.mat;
 
-% TODO: Extend calulation to more channel(Group(s))
-channel='F3';
-channel_index=strcmpi(channel, [EEG.channels.labels]);
-result=[];
-welch.kernel.window_size=2*EEG.srate;
-for trial_index=1:EEG.active.trialsCount
-    % Skip computation for trials that should be excluded
-    if ismember(trial_index, EEG.active.ec.excluded_trials)
-        continue;
-    end
-    % Calulate Welch method based on params
-    welch.signal.data=squeeze(EEG.active.ec.data(1, trial_index, channel_index, :))';
-    welch.kernel.data=hamming(welch.kernel.window_size);
-    [welch.PSD_estimate, welch.cyclical_frequencies]=pwelch(welch.signal.data, welch.kernel.data, round(welch.kernel.window_size/4), EEG.srate*100, EEG.srate);
-    % Extract Alpha band power and stash it into an array that is named result
-    index8=dsearchn(welch.cyclical_frequencies, 8);
-    index12=dsearchn(welch.cyclical_frequencies, 12);
-    result(end+1)=sum(welch.PSD_estimate(index8:index12));
-end
-% Write response(Alpha band powers) into a file
-fileID=fopen("result\power\active_ec\Alpha.txt", "w");
-fprintf(fileID, '%5d \n', result);
-fclose(fileID);
+group_names=["first", "second"];
+group_file_names=["Group1", "Group2"];
+pre_post_folder_names=["pre\", "post\"];
+groups.first.channels.labels=["F3", "Fz", "Fp1"];
+groups.second.channels.labels=["P3", "T5", "T3"];
+groups.first.channels.indexs= zeros(1, length(groups.first.channels.labels));
+groups.second.channels.indexs=zeros(1, length(groups.second.channels.labels));
 
-% TODO: do the same process for trials that are stored in 'post' folder
+% Calculate the index of each channel and also store it into an array
+for list_index=1:length(groups.first.channels.labels)
+    groups.first.channels.indexs(list_index)=find(strcmpi(groups.first.channels.labels(list_index), [EEG.channels.labels])==1);
+end
+for list_index=1:length(groups.second.channels.labels)
+    groups.second.channels.indexs(list_index)=find(strcmpi(groups.second.channels.labels(list_index), [EEG.channels.labels])==1);
+end
+
+welch.kernel.window_size=2*EEG.srate;
+for pre_post_index=1:2
+    for group_name_indexs=1:length(group_names)
+        result=[];
+        for trial_index=1:EEG.active.trialsCount
+            % Skip computation for trials that should be excluded
+            if ismember(trial_index, EEG.active.ec.excluded_trials)
+                continue;
+            end
+            % Calulate Welch method based on params
+            welch.signal.data=squeeze(mean(EEG.active.ec.data(pre_post_index, trial_index, groups.(group_names(group_name_indexs)).channels.indexs , :), 3))';
+            welch.kernel.data=hamming(welch.kernel.window_size);
+            [welch.PSD_estimate, welch.cyclical_frequencies]=pwelch(welch.signal.data, welch.kernel.data, round(welch.kernel.window_size/4), EEG.srate*100, EEG.srate);
+            % Extract Alpha band power and stash it into an array that is named result
+            index8=dsearchn(welch.cyclical_frequencies, 8);
+            index12=dsearchn(welch.cyclical_frequencies, 12);
+            result(end+1)=sum(welch.PSD_estimate(index8:index12));
+        end
+        % Write response(Alpha band powers) into a file
+        fileID=fopen(strcat("result\power\active_ec\",pre_post_folder_names(pre_post_index), group_file_names(group_name_indexs), "-Alpha.txt"), "w");
+        fprintf(fileID, '%5d \n', result);
+        fclose(fileID);
+    end
+end
