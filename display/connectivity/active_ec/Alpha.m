b30ead0e698fb463568600de926e21cfacf34b56
Omid Reza <omid77.orh@gmail.com>
2022-07-17 16:42:01 +0430

    Refactor
    Fix plot variable's name
    Add figure function to separate plots

--- display/connectivity/active_ec/Alpha.m
+++ display/connectivity/active_ec/Alpha.m
@@ -1,20 +1,21 @@
 clear;
-% Config plot
-plot.dot_size=100;
-plot.y.pre.data=load("result\connectivity\active_ec\pre\Alpha.txt");
-plot.y.post.data=load("result\connectivity\active_ec\post\Alpha.txt");
+% Config plt
+plt.dot_size=100;
+plt.y.pre.data=load("result\connectivity\active_ec\pre\Alpha.txt");
+plt.y.post.data=load("result\connectivity\active_ec\post\Alpha.txt");
 
-plot.y.pre.color=[57 62 70].*4/1000;
-plot.y.post.color=[250 119 0].*4/1000;
-plot.x.pre.data=ones(1, length(plot.y.pre.data));
-plot.x.post.data=ones(1, length(plot.y.post.data))+1;
-% Draw plots
+plt.y.pre.color=[57 62 70].*4/1000;
+plt.y.post.color=[250 119 0].*4/1000;
+plt.x.pre.data=ones(1, length(plt.y.pre.data));
+plt.x.post.data=ones(1, length(plt.y.post.data))+1;
+% Draw plts
+figure(1),clf;
 hold on;
 grid on;
-swarmchart(plot.x.pre.data, plot.y.pre.data, plot.dot_size, plot.y.pre.color, "filled");
-swarmchart(plot.x.post.data, plot.y.post.data, plot.dot_size, plot.y.post.color, "filled");
+swarmchart(plt.x.pre.data, plt.y.pre.data, plt.dot_size, plt.y.pre.color, "filled");
+swarmchart(plt.x.post.data, plt.y.post.data, plt.dot_size, plt.y.post.color, "filled");
 legend("Pre", "Post");
-% Config plot
+% Config plt
 set(gca,'XTick',[]);
 ylabel("Alpha Connectivity", "Color", [21 19 60].*4/1000, "FontWeight", "bold");
 set(gcf, "NumberTitle", false, "Name", "Active - EC");
--- display/power/active_ec/Alpha.m
+++ display/power/active_ec/Alpha.m
@@ -1,38 +1,38 @@
 clear;
-% Config plot
-plot.dot_size=100;
-plot.y.pre.group1.data=load("result\power\active_ec\pre\Group1-Alpha.txt");
-plot.y.pre.group2.data=load("result\power\active_ec\pre\Group2-Alpha.txt");
-plot.y.post.group1.data=load("result\power\active_ec\post\Group1-Alpha.txt");
-plot.y.post.group2.data=load("result\power\active_ec\post\Group2-Alpha.txt");
+% Config plt
+plt.dot_size=100;
+plt.y.pre.group1.data=load("result\power\active_ec\pre\Group1-Alpha.txt");
+plt.y.pre.group2.data=load("result\power\active_ec\pre\Group2-Alpha.txt");
+plt.y.post.group1.data=load("result\power\active_ec\post\Group1-Alpha.txt");
+plt.y.post.group2.data=load("result\power\active_ec\post\Group2-Alpha.txt");
 
-plot.y.pre.color=[57 62 70].*4/1000;
-plot.y.post.color=[250 119 0].*4/1000;
-plot.x.pre.data=ones(1, length(plot.y.pre.group1.data));
-plot.x.post.data=ones(1, length(plot.y.post.group1.data))+1;
-% Draw a plot for group1
-figure(1), clf;
-subplot(211)
+plt.y.pre.color=[57 62 70].*4/1000;
+plt.y.post.color=[250 119 0].*4/1000;
+plt.x.pre.data=ones(1, length(plt.y.pre.group1.data));
+plt.x.post.data=ones(1, length(plt.y.post.group1.data))+1;
+% Draw a plt for group1
+figure(21), clf;
+subplot(2, 1, 1)
 hold on;
 grid on;
-swarmchart(plot.x.pre.data, plot.y.pre.group1.data, plot.dot_size, plot.y.pre.color, "filled");
-swarmchart(plot.x.post.data, plot.y.post.group1.data, plot.dot_size, plot.y.post.color, "filled");
+swarmchart(plt.x.pre.data, plt.y.pre.group1.data, plt.dot_size, plt.y.pre.color, "filled");
+swarmchart(plt.x.post.data, plt.y.post.group1.data, plt.dot_size, plt.y.post.color, "filled");
 legend("Pre", "Post");
-% Config plot for group1
+% Config plt for group1
 set(gca,'XTick',[]);
 ylabel("Alpha Power", "Color", [21 19 60].*4/1000, "FontWeight", "bold");
 set(gcf, "NumberTitle", false, "Name", "Active - EC");
 set(gca, 'GridColor', [26 77 46].*4/1000);
 set(gca, 'YColor', [26 77 46].*4/1000);
 title('The First Group(F3, Fz, Fp1)');
-% Draw a plot for group2
-subplot(212);
+% Draw a plt for group2
+subplot(2, 1, 2);
 hold on;
 grid on;
-swarmchart(plot.x.pre.data, plot.y.pre.group2.data, plot.dot_size, plot.y.pre.color, "filled");
-swarmchart(plot.x.post.data, plot.y.post.group2.data, plot.dot_size, plot.y.post.color, "filled");
+swarmchart(plt.x.pre.data, plt.y.pre.group2.data, plt.dot_size, plt.y.pre.color, "filled");
+swarmchart(plt.x.post.data, plt.y.post.group2.data, plt.dot_size, plt.y.post.color, "filled");
 legend("Pre", "Post");
-% Config plot for group1
+% Config plt for group1
 set(gca,'XTick',[]);
 ylabel("Alpha Power", "Color", [21 19 60].*4/1000, "FontWeight", "bold");
 set(gcf, "NumberTitle", false, "Name", "Active - EC");
