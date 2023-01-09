bceb37459f6c5f062974adc066286e4e1ceb7cb4
omid-reza <omid77.orh@gmail.com>
2023-01-09 12:17:06 +0330

    Create Statistical Exporter

---
+++ src/statistics/Exporter.m
@@ -0,0 +1,41 @@
+clear;
+addpath(genpath('src/statistics'));
+inputArguments=ArgumentsLoader;
+
+%Power and Amplitude
+types = ["Amplitude", "Power"];
+params = ["active_mean", "sham_mean", "active_stderror", "sham_stderror", "p_value"];
+for type_index=1:length(types)
+    type=load(strcat("result\statistical\", types(type_index),".mat"));
+    for ec_eo=inputArguments.ec_eo
+        for freq=inputArguments.files
+            for net=inputArguments.networkGroups
+                fileID=fopen(strcat("result\statistical\", types(type_index),"\", ec_eo, "_", freq, "_", replace(net, "-", ""),".txt"), "w");
+                index=strcat(ec_eo, "_", freq, "_", replace(net, "-", ""));
+                for param_index=1:length(params)
+                    param=params(param_index);
+                    fprintf(fileID, '%s :', param);
+	                fprintf(fileID, '%f \n', type.result.(index).(param));
+                end
+                fclose(fileID);
+            end
+        end
+    end
+end
+
+%Connectivity
+type=load(strcat("result\statistical\Connectivity.mat"));
+for ec_eo=inputArguments.ec_eo
+    for freq=inputArguments.files
+        for net=inputArguments.networkGroups
+            fileID=fopen(strcat("result\statistical\Connectivity\", ec_eo, "_", freq, ".txt"), "w");
+            index=strcat(ec_eo, "_", freq, "_Total");
+            for param_index=1:length(params)
+                param=params(param_index);
+                fprintf(fileID, '%s :', param);
+                fprintf(fileID, '%f \n', type.result.(index).(param));
+            end
+            fclose(fileID);
+        end
+    end
+end
