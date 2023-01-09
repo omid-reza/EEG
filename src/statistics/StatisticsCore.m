3f220202cb247ed35dfdd9f6b2dcddf52e3a4e2a
omid-reza <omid77.orh@gmail.com>
2023-01-09 09:01:22 +0330

    update statistical outputs

--- src/statistics/StatisticsCore.m
+++ src/statistics/StatisticsCore.m
@@ -1,16 +1,9 @@
 function result = StatisticsCore(data)
     % mean for each group
-	result.active.mean=mean(data.active);
-	result.sham.mean=mean(data.sham);
+	result.active_mean=mean(data.active);
+	result.sham_mean=mean(data.sham);
     % Standard error for each group
-	result.active.stderror=std(data.active)/sqrt(length(data.active));
-	result.sham.stderror=std(data.sham)/sqrt(length(data.sham));
-    % F-value
-	if length(data.sham)>length(data.active)
-	    result.f_value=var(data.sham)/var(data.active);
-	else
-	    result.f_value=var(data.active)/var(data.sham);
-    end
+	result.active_stderror=std(data.active)/sqrt(length(data.active));
+	result.sham_stderror=std(data.sham)/sqrt(length(data.sham));
 	[result.h, result.p_value]=ttest(data.active, data.sham); % P-value
-    result.effect=meanEffectSize(data.active, data.sham); % f^2
 end
